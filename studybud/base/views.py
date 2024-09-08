3e3489c5c0bba19a9ee39e5e57e46adce0d260a4
Nick-Petruccelli <NickPetro18@gmail.com>
2024-09-08 14:16:19 -0400

    Update and delete rooms

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 from .models import Room
 from .forms import RoomForm
 
-rooms = Room.objects.all()
+
 # Create your views here.
 def home(request) -> HttpResponse:
+    rooms = Room.objects.all()
     return render(request, "home.html", {"rooms":rooms})
 
 def room(request, pk) -> HttpResponse:
@@ -25,3 +26,22 @@ def create_room(request):
             return redirect("/")
     context = {'form': form}
     return render(request, "create_room.html", context)
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form': form}
+    return render(request, "create_room.html", context)
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('/')
+    context = {'obj': 'room'}
+    return render(request, "delete.html", context)
