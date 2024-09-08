98b4954fa2daf7f57f048fa4e27894a80b4b4d3d
Nick-Petruccelli <NickPetro18@gmail.com>
2024-09-08 12:19:46 -0400

    Database and models

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=20)
+    desc = models.TextField(null=True, blank=True)
+    last_update = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    msg = models.TextField()
+
+    def __str__(self) -> str:
+        return self.msg[0:50]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
-rooms = [
-    {"id": 1, "name":"room1"},
-    {"id": 2, "name":"room2"},
-    {"id": 3, "name":"room3"},
-]
+rooms = Room.objects.all()
 # Create your views here.
 def home(request) -> HttpResponse:
     return render(request, "home.html", {"rooms":rooms})
@@ -13,7 +10,7 @@ def home(request) -> HttpResponse:
 def room(request, pk) -> HttpResponse:
     room = None
     for i in rooms:
-        if i['id'] == pk:
+        if i.id == pk:
             room = i
     context = {'room': room}
     return render(request, "room.html", context=context)
