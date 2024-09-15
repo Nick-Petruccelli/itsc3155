042d5a85a6de7e9a9c95fd4a720d5bcbb963fb2b
Nick-Petruccelli <NickPetro18@gmail.com>
2024-09-15 17:20:09 -0400

    search

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 
 # Create your views here.
 def home(request) -> HttpResponse:
-    rooms = Room.objects.all()
-    return render(request, "home.html", {"rooms":rooms})
+    q = request.GET.get('q')
+    if q:
+        rooms = Room.objects.filter(
+            Q(topic__name__icontains=q) |
+            Q(name__icontains=q) |
+            Q(desc__icontains=q)
+            )
+    else:
+        rooms = Room.objects.all()
+    room_count = rooms.count()
+    topics = Topic.objects.all()
+    context = {"rooms":rooms, "topics":topics, "room_count":room_count}
+    return render(request, "home.html", context)
 
 def room(request, pk) -> HttpResponse:
+    rooms = Room.objects.all()
     room = None
     for i in rooms:
         if i.id == pk:
